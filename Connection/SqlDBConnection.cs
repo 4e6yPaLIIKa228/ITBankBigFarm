021fe3250f4a78d378843adb618aa576cec3bd30
4e6yPaLIIKa228 <alexander_pushkar@mail.ru>
2022-04-22 14:04:04 +0300

    Добавьте файлы проекта.

---
+++ Connection/SqlDBConnection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITBankBigFarm.Connection
+{
+    class SqlDBConnection
+    {
+        public static string connection =
+                  "Network Library=DBMSSOCN;" +
+                  "Data Source=192.168.50.136;" +
+                  "Initial Catalog=PushkarevDB;" +
+                  "User Id=Pushkarev;" +
+                  "Password=QWEasd123;";
+    }
+}
---
+++ Windows/RegistWin.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.Security.Cryptography;
+
+
+
+namespace ITBankBigFarm.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistWin.xaml
+    /// </summary>
+    public partial class RegistWin : Window
+    {
+        public RegistWin()
+        {
+            InitializeComponent();
+        }
+     
+        public void Checker()
+        {
+
+        }
+
+        private void btnReg_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
+
