18bd3e9c59c0756b0d62c09da8d3486bdabf7d36
fcscxzvcz <rtfegdsgfv>
2022-05-06 13:49:02 +0300

    test

--- Windows/MenuBank.xaml.cs
+++ Windows/MenuBank.xaml.cs
@@ -15,6 +15,7 @@ using ITBankBigFarm.Windows;
 using ITBankBigFarm.Connection;
 using System.Data.SqlClient;
 using System.Data.SQLite;
+using System.Data;
 
 namespace ITBankBigFarm.Windows
 {
@@ -23,11 +24,14 @@ namespace ITBankBigFarm.Windows
     /// </summary>
     public partial class MenuBank : Window
     {
+        int NumberFaceFiz, NumberFaceYur;
         public MenuBank()
         {
             InitializeComponent();
-            Load();
+            LoadHome();
             Profile.Visibility = Visibility.Hidden;
+            LoadProfelComb();
+            
         }
 
         private void imgclouse_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -35,7 +39,7 @@ namespace ITBankBigFarm.Windows
             Environment.Exit(0);
         }
 
-        public void Load()
+        public void LoadHome()
         {
             try
             {
@@ -78,5 +82,96 @@ namespace ITBankBigFarm.Windows
             this.Close();
             Aftoriz.ShowDialog();
         }
+
+        public void LoadProfelComb() //данные для комбобокса
+        {
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(SqlDBConnection.connection))
+                {
+                    connection.Open();
+                    string query = $@"SELECT * FROM Pols";
+                    SQLiteCommand cmd = new SQLiteCommand(query, connection);
+                    SQLiteDataAdapter SDA = new SQLiteDataAdapter(cmd);
+                    DataTable dt = new DataTable("Pols");
+                    SDA.Fill(dt);
+                    cmbPols.ItemsSource = dt.DefaultView;
+                    cmbPols.DisplayMemberPath = "Pol";
+                    cmbPols.SelectedValuePath = "ID";
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ошибка" + ex);
+            }
+
+        }
+
+       
+
+        private void cmbFace_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbFace.SelectedIndex == 0)
+            {
+                MessageBox.Show("Физ");
+               // NumberFaceFiz = 1;
+                FizFace();
+            }
+            else
+            {
+                NumberFaceFiz = 0;
+            }
+            if (cmbFace.SelectedIndex == 1)
+            {
+                MessageBox.Show("Юр");
+                NumberFaceYur = 1;
+            }
+            else
+            {
+                NumberFaceYur = 0;
+            }
+        }
+        public void FizFace()
+        {
+          // if(NumberFaceFiz == 1)
+
+          //  {
+                try
+                {
+                    using (SQLiteConnection connection = new SQLiteConnection(SqlDBConnection.connection))
+                    {
+                        connection.Open();
+                        string query = $@"SELECT COUNT(1) FROM PhysicalPerson WHERE ID = {Saver.IDAcc};"; //Получение данных из таблицы Девайсы
+                        SQLiteCommand cmd = new SQLiteCommand(query, connection); 
+                       // cmd3.Parameters.AddWithValue("IDBrand", IdKab);
+                        int count = Convert.ToInt32(cmd.ExecuteScalar());
+                        if (count == 1)
+                        {
+                            MessageBox.Show("1");
+                            query = "SELECT Name,Family FROM PhysicalPerson WHERE ID = 1;";
+                            SQLiteDataReader dr = null;
+                            SQLiteCommand cmd1 = new SQLiteCommand(query, connection);
+                            dr = cmd1.ExecuteReader();
+                            while (dr.Read())
+
+                        {
+
+                            txtfame.Text = dr["Family"].ToString();
+                            txtname.Text = dr["Name"].ToString();
+
+                        }
+                    }
+                        else
+                        {
+                            MessageBox.Show("0");
+                        }
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Ошибка" + ex);
+                }
+            } 
+        //}
     }
 }
